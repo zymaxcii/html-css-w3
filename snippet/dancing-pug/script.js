4bc5df1c9d6a4a46ca3547f4de504004d03f58da
zymaxcii <zyma.xcii@gmail.com>
2021-07-03 21:08:58 +0800

    added pug stuff

---
+++ snippet/dancing-pug/script.js
@@ -0,0 +1,7 @@
+// Design made by Angela Velasquez, me :D.
+
+// It also was a challenge accepted on the UX Enpterprise Team: PUX
+
+//I took the original idea from https://www.goabstract.com 
+
+// July 2017.
